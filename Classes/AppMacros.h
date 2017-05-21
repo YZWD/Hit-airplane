90e79b6ec8131d143eb78405c53932d338dede18
YZWD <YZWD@users.noreply.github.com>
2017-05-21 12:47:13 +0800

    Create AppMacros.h

---
+++ Classes/AppMacros.h
@@ -0,0 +1,32 @@
+#ifndef __APPMACROS_H__
+#define __APPMACROS_H__
+// Hit-airplane
+#include "cocos2d.h"
+#define DESIGN_RESOLUTION_480X320    0
+#define DESIGN_RESOLUTION_1024X768   1
+#define DESIGN_RESOLUTION_2048X1536  2
+#define TARGET_DESIGN_RESOLUTION_SIZE  DESIGN_RESOLUTION_480X320
+
+typedef struct tagResource
+{
+    cocos2d::CCSize size;
+    char directory[100];
+}Resource;
+
+static Resource smallResource  =  { cocos2d::CCSizeMake(480, 320),   "iphone" };
+static Resource mediumResource =  { cocos2d::CCSizeMake(1024, 768),  "ipad"   };
+static Resource largeResource  =  { cocos2d::CCSizeMake(2048, 1536), "ipadhd" };
+
+#if (TARGET_DESIGN_RESOLUTION_SIZE == DESIGN_RESOLUTION_480X320)
+static cocos2d::CCSize designResolutionSize = cocos2d::CCSizeMake(480, 320);
+#elif (TARGET_DESIGN_RESOLUTION_SIZE == DESIGN_RESOLUTION_1024X768)
+static cocos2d::CCSize designResolutionSize = cocos2d::CCSizeMake(1024, 768);
+#elif (TARGET_DESIGN_RESOLUTION_SIZE == DESIGN_RESOLUTION_2048X1536)
+static cocos2d::CCSize designResolutionSize = cocos2d::CCSizeMake(2048, 1536);
+#else
+#error unknown target design resolution!
+#endif
+
+#define TITLE_FONT_SIZE  (cocos2d::CCEGLView::sharedOpenGLView()->getDesignResolutionSize().width / smallResource.size.width * 24)
+
+#endif
